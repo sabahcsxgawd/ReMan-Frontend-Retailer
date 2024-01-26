aa3dfb092d550dc8ef671f1a6402a373072baff7
sabahcsxgawd <sabah72224@gmail.com>
2024-01-27 00:11:58 +0600

    Update build and preview ports in package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 4000,
+  },
+  preview: {
+    port: 8080,
+  },
 })
