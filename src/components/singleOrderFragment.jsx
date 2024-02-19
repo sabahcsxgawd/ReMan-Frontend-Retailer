affc412bac5feb0f5f177a71430ec41640aa4e48
sabahcsxgawd <sabah72224@gmail.com>
2024-02-19 13:04:29 +0600

    fixed delete method data passing

--- src/components/singleOrderFragment.jsx
+++ src/components/singleOrderFragment.jsx
@@ -25,7 +25,7 @@ export default function SingleOrderFragment({ order }) {
         const apiUrl = `${import.meta.env.VITE_API_URL}/cart/deleteProduct`;
 
         try {
-            const response = await axios.delete(apiUrl, postData);
+            const response = await axios.delete(apiUrl, {data: postData});
             if (response.status === 201) {
                 alert('Product deleted from cart');
                 window.location.reload();
